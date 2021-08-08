1ac626c17760dbca4e0900cb7dace5a1a70c6ef3
Rosie <rosanna.rossington@ynap.com>
2021-08-08 23:29:03 +0100

    structure

---
+++ api/index.js
@@ -0,0 +1,37 @@
+//value of express package 
+const express = require('express');
+// and initialize
+const app = express()
+const PORT = 8080;
+
+//apply middleware
+app.use(express.json())
+//fire up api on server
+app.listen(PORT, () => console.log(`Running on ${PORT}`));
+
+//Cannot GET  because no api end points
+
+
+
+app.get('/poses', (req, res) => {
+  //callback to handle request
+  res.status(200).send({
+    tshirt: '',
+    size: 'xl',
+  });
+});
+
+//dynamic url params a represenation to handle all diffetent in a function
+app.post('/classes/:id', (req, res) => {
+  const { id } = req.params;
+  const { logo } = req.body;
+  //express does not parse json as default 
+  //need middleware to tell express to parse json bedofre data hits the function to handle request 
+
+  if (!logo) {
+    res.status(418).send({ message: 'We need a logo' });
+  }
+  res.send({
+    tshirt: `${logo} ${id}`,
+  });
+});
